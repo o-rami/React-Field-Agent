317e17d8d6f5f807c53e42386b032d390a65df16
Oscar Ramirez <oramirezent@gmail.com>
2023-06-16 15:37:33 -0400

    AgentApi.js Compl

--- client/react-field-agent/src/Services/AgentApi.js
+++ client/react-field-agent/src/Services/AgentApi.js
@@ -9,27 +9,16 @@ export async function findAllAgents() {
 };
 
 export async function findAgentById(id) {
-
+  const reponse = await fetch(`${AGENT_API_URL}/${id}`);
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    return Promise.reject(`Agent with ID#:${id} was not found`);
+  }
 
 }
 
 export async function createAgent(agent) {
-
-
-}
-
-export async function updateAgent(agent) {
-
-}
-
-export async function deleteAgentById(id) {
-
-}
-
-
-
-
-const add = async (agent) => {
   const init = makeAgentInit('POST', agent);
 
   const response = await fetch(AGENT_API_URL, init);
@@ -39,41 +28,33 @@ const add = async (agent) => {
     return Promise.reject(errors);
   }
   return response.json();
-};
+}
 
-const update = async (agent) => {
+export async function updateAgent(agent) {
   const init = makeAgentInit('PUT', agent);
 
   const url = `${AGENT_API_URL}/${agent.agentId}`;
-  console.log(url);
-
 
   const response = await fetch(url, init);
 
   if (response.status === 404) {
-    return Promise.reject(`${agent.firstName} ${agent.lastName} was not found!`);
+    return Promise.reject(`Agent ${agent.firstName} ${agent.lastName} was not found!`);
   } else if (response.status !== 204) {
     const errors = await response.json();
     return Promise.reject(errors);
   }
+}
+
+export async function deleteAgentById(id) {
+
+  const response = await fetch(`${AGENT_API_URL}/${id}`, { method: 'DELETE' });
+
+  if (response.status === 404) {
+    return Promise.reject(`Agent with ID# ${id}was not found.`)
+  }
 };
 
-const deleteById = async (agent) => {
-  fetch(`${AGENT_API_URL}/${agent.agentId}`, { method: 'DELETE', })
-    .then(response => {
-      if (response.status === 204) {
-        //set
-      } else if (response.status === 404) {
-        return Promise.reject(`Agent ${agent.firstName} ${Agent.lastName} not found`);
-      } else {
-        return Promise.reject(`Delete failed with status: ${reponse.status}`)
-      }
-    })
-    .catch(console.log);
-
-};
-
-const makeAgentInit = (method, agent) => {
+function makeAgentInit(method, agent) {
   return {
     method: method,
     headers: {
