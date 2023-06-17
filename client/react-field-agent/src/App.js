fdca0c1870dd521fc90ef7c274e5b702c6b9f101
Oscar Ramirez <oramirezent@gmail.com>
2023-06-17 12:08:44 -0400

    Starting CRUD

--- client/react-field-agent/src/App.js
+++ client/react-field-agent/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import AgentList from "./Components/AgentList";
+import AgentForm from "./Components/AgentForm";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
         <Route
           path="/"
           element={<AgentList />} />
+          <Route
+          path="/add"
+          element={<AgentForm />} />
         {/* <Route
           path="/agents/edit/:id"
           element={<AgentForm />} />
